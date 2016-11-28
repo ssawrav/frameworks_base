ade79c58761ce1bbf860e9ca069bb9dd372f3700
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2016-11-27 21:35:10 -0600

    fix build * Remove trusted flags * remove some things not defined
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -62,11 +62,7 @@
 #include "audioplay.h"
 
 #include <private/regionalization/Environment.h>
-#define OEM_BOOTANIMATION_FILE "/oem/media/bootanimation.zip"
-#define SYSTEM_BOOTANIMATION_FILE "/system/media/bootanimation.zip"
-#define SYSTEM_ENCRYPTED_BOOTANIMATION_FILE "/system/media/bootanimation-encrypted.zip"
 #define THEME_BOOTANIMATION_FILE "/data/system/theme/bootanimation.zip"
-#define EXIT_PROP_NAME "service.bootanim.exit"
 
 namespace android {
 
@@ -418,13 +414,12 @@ status_t BootAnimation::readyToRun() {
     }
     else if (access(getAnimationFileName(IMG_SYS), R_OK) == 0) {
         mZipFileName = getAnimationFileName(IMG_SYS);
-    }
+	}
     else if (access(THEME_BOOTANIMATION_FILE, R_OK) == 0) {
         mZipFileName = THEME_BOOTANIMATION_FILE;
     }
     else if (access(OEM_BOOTANIMATION_FILE, R_OK) == 0) {
         mZipFileName = OEM_BOOTANIMATION_FILE;
-
     }
 
 #ifdef PRELOAD_BOOTANIMATION
